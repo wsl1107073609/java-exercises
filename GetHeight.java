d53790bde4c1a9ca380f64e01cd464534f8aee07
Cherry_wu <1107073609@qq.com>
2016-03-31 17:33:53 +0800

    Update GetHeight.java

--- GetHeight.java
+++ GetHeight.java
@@ -29,16 +29,7 @@ public class GetHeight {
 		}
 		*/
 		
-		/*
-		* method2:
-		double h = 100,s = 100;
-		for(int i=1; i<10; i++) {
-		   s = s + h;
-		   h = h / 2;
-		}
-		System.out.println("经过路程：" + s);
-		System.out.println("反弹高度：" + h / 2);
-		*/
+		
 	}
 
 }
